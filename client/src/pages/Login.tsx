a193435558f7e0e17dd05cedf24b6aaa63500fc1
Ayhan Tefik <ayhantefik93@gmail.com>
2026-04-02 11:23:35 +0200

    Login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
+import '../styles/Login.css';
+
 function Login() {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [rememberMe, setRememberMe] = useState<boolean>(false);
+
+    const handleLogin = () => {
+        console.log("email ", email);
+        console.log("password ", password);
+    }
     return(
-        <>
-            Login
-        </>
+        <div className="login">
+            <h2>Login</h2>
+            <div className="login-form">
+                <form>
+                    <label>Email
+                        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </label>
+                    <label>Password
+                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </label>
+                    <label>Remember me
+                        <input type="checkbox" onChange={() => setRememberMe(!rememberMe)} />
+                    </label>
+                    <button onClick={handleLogin}>Login</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
