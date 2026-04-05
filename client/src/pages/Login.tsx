f2a87e35b24e9ca7b7551cb0b8a04f3744ef151e
Ayhan Tefik <ayhantefik93@gmail.com>
2026-04-05 20:28:24 +0200

    setup axios and connect to API using .env

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../api/api";
 import '../styles/Login.css';
 
 function Login() {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
     const [rememberMe, setRememberMe] = useState<boolean>(false);
+    const navigate = useNavigate();
 
-    const handleLogin = () => {
-        console.log("email ", email);
-        console.log("password ", password);
+    const handleLogin = async () => {
+        try {
+            await api.post('/login', { email, password });
+
+            navigate('/'); // Navigate to index if status is 200
+        } catch (error) {
+            console.error(error);
+        }
     }
     return(
         <div className="login">
@@ -24,7 +32,7 @@ function Login() {
                     <label>Remember me
                         <input type="checkbox" onChange={() => setRememberMe(!rememberMe)} />
                     </label>
-                    <button onClick={handleLogin}>Login</button>
+                    <button type="button" onClick={handleLogin}>Login</button>
                 </form>
             </div>
         </div>
