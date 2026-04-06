25d4fd994021bc390553ef453aabaaf50ae3fb2a
Ayhan Tefik <ayhantefik93@gmail.com>
2026-04-06 14:45:20 +0200

    PR changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import EmployeeList from "./pages/EmployeeList";
